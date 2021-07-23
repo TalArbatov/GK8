97817c204149633085135832506017af419815b6
Tal Arbatov <talarbatov98@gmail.com>
2021-07-23 14:56:02 +0300

    correctly parsed API response

--- src/Entry.js
+++ src/Entry.js
@@ -16,17 +16,17 @@ const Entry = () => {
       method: 'GET',
       protocol: 'https:',
       hostname: 'api.etherscan.io',
-      path: '/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&sort=asc&apikey=YourApiKeyToken',
+      path: '/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&sort=asc&apikey=MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP',
       redirect: 'follow'
     });
 
     request.on('response', (response) => {
-      console.log(`STATUS: ${response.statusCode}`);
-      console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+      // console.log(`STATUS: ${response.statusCode}`);
+      // console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
 
       response.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`)
-          setTransactions(chunk);
+          console.log(`BODY: ${JSON.parse(chunk).result}`)
+          setTransactions(JSON.parse(chunk).result);
       });
   });
     request.setHeader('Content-Type', 'application/json');
@@ -36,7 +36,7 @@ const Entry = () => {
   return (
     <div className="wrapper">
       <p1>Hello world!</p1>
-      <TransactionList />
+      <TransactionList transactions={ transactions }/>
     </div>
   )
 };
