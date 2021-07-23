e9c84fdca845828f48cfa5a3734a55a8e377f77d
Tal Arbatov <talarbatov98@gmail.com>
2021-07-23 16:30:04 +0300

    small refactor

--- src/Entry.js
+++ src/Entry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import config from './config';
 import TransactionList from './TransactionList';
 const electron = window.require("electron")
@@ -11,54 +11,36 @@ const Entry = () => {
   const [error, setError] = useState({
     active: false,
     message: ''
-  })
+  });
 
   const sendRequest = () => {
-    const { key } = config.api;
-    const url = config.api.url(address, key);
-    console.log('tal test', url);
+    const { key, hostname, path } = config.api;
 
     const request = net.request({
       method: 'GET',
       protocol: 'https:',
-      hostname: 'api.etherscan.io',
-      path: `/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP`,
+      hostname,
+      path: path(address, key),
       redirect: 'follow'
     });
     request.on('response', (response) => {
-      // console.log(`STATUS: ${response.statusCode}`);
-      // console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
       response.on('data', (chunk) => {
           try {
-            console.log(`BODY: ${JSON.parse(chunk).result}`)
             const result = JSON.parse(chunk).result;
             if (!Array.isArray(result)) throw 'Invalid address';
             else if (Array.isArray(result) && result.length === 0) throw 'No transactions'
             else {
               setTransactions(result.slice(0, config.maxTransactions));
-              setError({
-                active: false,
-                message: ''
-              });
-          }
+              setError({ active: false, message: '' });
+            }
           } catch(e) {
-            console.log('not an array', e);
-            setError({
-              active: true,
-              message: e
-            })
+            setTransactions([]);
+            setError({ active: true, message: e })
           }
-          
       });
     });
     request.setHeader('Content-Type', 'application/json');
     request.end();
-  }
-
-
-
-  const search = () => {
-    sendRequest();
   }
 
   const handleChange = event => {
@@ -69,10 +51,10 @@ const Entry = () => {
     <div className="main">
       <div className="search">
         <input className="searchTerm" type="text" placeholder="Please enter wallet" onChange={ handleChange }/>
-        <button className="searchButton" onClick={ search }>Search</button>
+        <button className="searchButton" onClick={ sendRequest }>Search</button>
       </div>
       <TransactionList transactions={ transactions }/>
-      <p style={ { color: 'red' } }>{ error.active && error.message }</p>
+      <p className="error-message">{ error.active && error.message }</p>
     </div>
   )
 };
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   api: {
-    key: 'MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP',
-    url: (address, key) => `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${key}`,
+    key: 'MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP', // had trouble figuring how to implement dotenv
+    hostname: 'api.etherscan.io',
+    path: (address, key) => `/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${key}`
   },
   maxTransactions: 10000
 };
