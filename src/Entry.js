31e1537b97dd7a5f9470a545a28cdd52b3747a00
Tal Arbatov <talarbatov98@gmail.com>
2021-07-23 15:38:11 +0300

    generated input field + corresponding handler functions

--- src/Entry.js
+++ src/Entry.js
@@ -6,8 +6,14 @@ const electron = window.require("electron")
 const net = electron.remote.net;
 
 const Entry = () => {
+  const [address, setAddress] = useState('');
   const [transactions, setTransactions] = useState([]);
-  useEffect(() => {
+
+  // useEffect(() => {
+  //   sendRequest();
+  // }, [])
+
+  const sendRequest = () => {
     const { address, key } = config.api;
     const url = config.api.url(address, key);
     console.log('tal test', url);
@@ -16,25 +22,33 @@ const Entry = () => {
       method: 'GET',
       protocol: 'https:',
       hostname: 'api.etherscan.io',
-      path: '/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&sort=asc&apikey=MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP',
+      path: `/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=MZ4UW331HNIZZGB7SFM7N319NU4XQTICHP`,
       redirect: 'follow'
     });
-
     request.on('response', (response) => {
       // console.log(`STATUS: ${response.statusCode}`);
       // console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-
       response.on('data', (chunk) => {
           console.log(`BODY: ${JSON.parse(chunk).result}`)
           setTransactions(JSON.parse(chunk).result);
       });
-  });
+    });
     request.setHeader('Content-Type', 'application/json');
     request.end();
-  }, [])
+  }
+
+  const generate = () => {
+    sendRequest();
+  }
+
+  const handleChange = event => {
+    setAddress(event.target.value);
+  }
 
   return (
     <div className="wrapper">
+      <input type="text" placeholder="Please enter wallet" onChange={ handleChange }/>
+      <button onClick={ generate }>Generate</button>
       <TransactionList transactions={ transactions }/>
     </div>
   )
